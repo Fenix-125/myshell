227c4460a133a5c11519fc5e16a00cb9d40ac5f2
Mykola Morgunenko <Olli25010@gmail.com>
2020-09-30 16:44:29 +0300

    add myshell options parser

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,25 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <iostream>
+#include <filesystem>
 #include "shell.h"
+#include "options_parser.h"
 
-int main() {
-    loop();
+command_line_options parse_arguments(int argc, char **argv);
+
+int main(int argc, char **argv) {
+    command_line_options conf;
+    conf.parse(argc, argv);
+    if (conf.get_filenames().size() > 1) {
+        std::cerr << "too many arguments" << std::endl;
+    }
+    if (conf.get_filenames().empty()) {
+        loop();
+    } else if (std::filesystem::path(conf.get_filenames()[0]).extension() == ".msh") {
+        //TODO: launch script here
+    } else {
+        std::cerr << "Bad script extension (.msh required)" << std::endl;
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
